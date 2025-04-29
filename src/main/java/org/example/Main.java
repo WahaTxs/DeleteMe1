b31ed4dc1c97ff83292ba4a812f29a86cab6ca13
Amine Achik <2472608@edu.vaniercollege.qc.ca>
2025-04-29 15:21:11 -0400

    adding insertionSort to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -27,14 +27,26 @@ public class Main {
     public static void selectionSort(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
             int min = i;
-            for (int j = i + 1; j < nums.length;j++) {
-                if(nums[min] > nums[j]) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[min] > nums[j]) {
                     min = j;
                 }
             }
             int temp = nums[i];
             nums[i] = nums[min];
             nums[min] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
         }
     }
 }
