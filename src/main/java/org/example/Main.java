7633396cba998fe3ce9f8cfc52b2253377727878
Amine Achik <2472608@edu.vaniercollege.qc.ca>
2025-04-29 15:34:04 -0400

    modify the overload bubbleSort method alawing it to sort ascendigly or descendigly

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,17 @@ public class Main {
     public static void main(String[] args) {
 
         int[] nums = {3, 1, 5, 2, 1, 4};
-        bubbleSort(nums);
+        bubbleSort(nums , false);
         System.out.println(Arrays.toString(nums));
     }
+    public static void bubbleSort(int[] nums ) {
+        bubbleSort(nums,true);
+    }
 
-    public static void bubbleSort(int[] nums) {
+    public static void bubbleSort(int[] nums, boolean ascending) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = 0; j < nums.length - 1 - i; j++) {
-                if (nums[j] > nums[j + 1]) {
+                if (ascending && nums[j] > nums[j + 1] || !ascending && nums[j] < nums[j + 1]) {
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
