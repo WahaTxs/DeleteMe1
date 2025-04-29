2ba9a6ca755935e373696652bc5ab931897e121f
Amine Achik <2472608@edu.vaniercollege.qc.ca>
2025-04-29 14:42:16 -0400

    add bublesort method in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] nums = {3, 1, 5, 2, 1, 4};
+        bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+               if( nums[j] > nums[j+1]) {
+                   int temp = nums[j];
+                   nums[j] = nums[j + 1];
+                   nums[j + 1] = temp;
+               }
+            }
         }
+
     }
 }
